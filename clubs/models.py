b91b43d938dd841936fdefcf2fb43c34e66e381a
dzmitryi-lazarchyk <dzmitryi.lazarchyk1994@gmail.com>
2024-09-23 21:17:33 +0300

    Table, venue and club models.

--- clubs/models.py
+++ clubs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 class Member(models.Model):
     first_name = models.CharField(max_length=50)
@@ -7,3 +8,24 @@ class Member(models.Model):
 
     def __str__(self):
         return f'Member(id={self.id}, last_name={self.last_name})'
+
+class Club(models.Model):
+    name = models.CharField(max_length=20)
+    members = models.ManyToManyField(Member)
+
+    def __str__(self):
+        return f"Club(id={self.id}, name={self.name})"
+
+class Venue(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Venue(id={self.id}, name={self.name})"
+
+class Table(models.Model):
+    number = models.IntegerField(validators=[MinValueValidator(1)])
+    seats = models.PositiveIntegerField(validators=[MinValueValidator(2)])
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE, related_name='tables')
+
+    def __str__(self):
+        return f"Table(id={self.id}, number={self.number})"
