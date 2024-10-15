3863b042be641bfaf52e7e567bb97e9d45529dab
dzmitryi-lazarchyk <dzmitryi.lazarchyk1994@gmail.com>
2024-10-15 18:39:05 +0300

    Logout handling.

--- foodmates/settings.py
+++ foodmates/settings.py
@@ -126,3 +126,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Account Management
+LOGIN_REDIRECT_URL = '/clubs/clubs'
+LOGOUT_REDIRECT_URL = '/clubs/clubs'
