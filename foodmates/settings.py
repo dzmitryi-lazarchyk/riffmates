5725c621caf19f4e2f92e618b4926378248a936f
dzmitryi-lazarchyk <dzmitryi.lazarchyk1994@gmail.com>
2024-10-18 07:02:21 +0300

    Password reset via email.

--- foodmates/settings.py
+++ foodmates/settings.py
@@ -130,3 +130,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Account Management
 LOGIN_REDIRECT_URL = '/clubs/clubs'
 LOGOUT_REDIRECT_URL = '/clubs/clubs'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'xlimpx999@gmail.com'
+EMAIL_HOST_PASSWORD = 'sjmz yiwn rgxr twat'
+
