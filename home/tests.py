95c1064334856008610556ce896bad4fd18d65fd
dzmitryi-lazarchyk <dzmitryi.lazarchyk1994@gmail.com>
2024-11-19 05:54:40 +0300

    Credits view unit test.

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TestHome(TestCase):
+    def test_credits(self):
+        response = self.client.get("/credits/")
+        self.assertEqual(200, response.status_code)
